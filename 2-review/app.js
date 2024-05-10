66a428ef5d1190921ae3fa21469b0a21fbdafe62
Oliver Hogan <oliver@oliver-hogan.com>
2024-05-10 13:09:31 +1000

    finish review

--- 2-review/app.js
+++ 2-review/app.js
@@ -11,7 +11,7 @@ const reviews = [
     name: "Jane Boe",
     title: "UX Designer",
     img: "./media/woman1.jpg",
-    info: "fide cure appeal rin sakura aiming mda finalists evaluation reducing fucker novak composites trucos ticks goog foolishly meticulously lh booksellers alibaba deviation monsoon aztecs suspicious ashrae bursts tenants ests chrome adopt ing soiled bellaire cardiac feist bullard diminish chivalry convocation kitchens heater ellesmere tone administrivia amaya discoveries ethnically caliente kickin",
+    info: "fide cure appeal rin sakura aiming mda finalists evaluation reducing novak composites trucos ticks goog foolishly meticulously lh booksellers alibaba deviation monsoon aztecs suspicious ashrae bursts tenants ests chrome adopt ing soiled bellaire cardiac feist bullard diminish chivalry convocation kitchens heater ellesmere tone administrivia amaya discoveries ethnically caliente kickin",
   },
   {
     id: 3,
@@ -30,7 +30,31 @@ const reviewText = document.getElementById("review-text");
 const prevBtn = document.querySelector(".left");
 const nextBtn = document.querySelector(".right");
 
-let currentId = 0;
+let currentId = 1;
 window.addEventListener("DOMContentLoaded", () => {
-  alert("window loaded");
+  updateReview();
+});
+
+function updateReview() {
+  const item = reviews[currentId - 1];
+  img.src = item.img;
+  author.textContent = item.name;
+  jobTitle.textContent = item.title;
+  reviewText.textContent = item.info;
+}
+
+prevBtn.addEventListener("click", () => {
+  if (currentId === 1) {
+    return;
+  }
+  currentId--;
+  updateReview();
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currentId === reviews.length) {
+    return;
+  }
+  currentId++;
+  updateReview();
 });
